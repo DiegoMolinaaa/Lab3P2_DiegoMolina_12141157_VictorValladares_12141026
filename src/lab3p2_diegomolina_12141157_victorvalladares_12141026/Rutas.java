75dbf53431a4f3fa8890bc67cf8ccdd85a49d177
DiegoMolinaaa <diego.molina0801@gmail.com>
2022-02-04 14:29:46 -0600

    Se agregaron variables, constructores, mutadores y toString a clase Rutas

--- src/lab3p2_diegomolina_12141157_victorvalladares_12141026/Rutas.java
+++ src/lab3p2_diegomolina_12141157_victorvalladares_12141026/Rutas.java
@@ -10,5 +10,46 @@ package lab3p2_diegomolina_12141157_victorvalladares_12141026;
  * @author diego
  */
 public class Rutas {
+    private String nombreRuta;
+    private int x;
+    private int y;
+
+    public Rutas() {
+    }
+
+    public Rutas(String nombreRuta, int x, int y) {
+        this.nombreRuta = nombreRuta;
+        this.x = x;
+        this.y = y;
+    }
+
+    public String getNombreRuta() {
+        return nombreRuta;
+    }
+
+    public void setNombreRuta(String nombreRuta) {
+        this.nombreRuta = nombreRuta;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "Rutas:" + "Nombre de Ruta = " + nombreRuta + ", Coordenada X = " + x + ", coordenada Y = " + y;
+    }
     
 }
