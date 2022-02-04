c0222d4480b7995050528f1feb83534be6c61ea0
DiegoMolinaaa <diego.molina0801@gmail.com>
2022-02-04 14:21:33 -0600

    Se agregaron variables, mutadores, constructores y to String a clase Personas, Transportistas y Alumnos

--- src/lab3p2_diegomolina_12141157_victorvalladares_12141026/Transportistas.java
+++ src/lab3p2_diegomolina_12141157_victorvalladares_12141026/Transportistas.java
@@ -5,10 +5,46 @@
  */
 package lab3p2_diegomolina_12141157_victorvalladares_12141026;
 
+import java.util.Date;
+
 /**
  *
  * @author diego
  */
-public class Transportistas {
+public class Transportistas extends Personas{
+    private int aniosExperiencia;
+    private String apodo;
+
+    public Transportistas() {
+        super();
+    }
+
+    public Transportistas(int aniosExperiencia, String apodo, String nombre, int identidad, Date fechaNacimiento) {
+        super(nombre, identidad, fechaNacimiento);
+        this.aniosExperiencia = aniosExperiencia;
+        this.apodo = apodo;
+    }
+
+    public int getAniosExperiencia() {
+        return aniosExperiencia;
+    }
+
+    public void setAniosExperiencia(int aniosExperiencia) {
+        this.aniosExperiencia = aniosExperiencia;
+    }
+
+    public String getApodo() {
+        return apodo;
+    }
+
+    public void setApodo(String apodo) {
+        this.apodo = apodo;
+    }
+
+    @Override
+    public String toString() {
+        return "Transportistas: "+super.toString()+ "Años de Experiencia = " + aniosExperiencia + ", Apodo=" + apodo;
+    }
+    
     
 }
